a7498d18691b505f0833112dd3734c06a80e4c97
arthurfurtunato <arthurfurtunato@gmail.com>
2023-02-23 11:57:51 -0300

    feat: adding the controller and the routes, making each file with its own function

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import db from './config/dbConnect.js';
 import livros from './models/livro.js';
+import routes from './routes/index.js'
 
 db.on("error", console.log.bind(console, 'Erro de conexão...'))
 db.once("open", () => {
@@ -11,43 +12,6 @@ const app = express();
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.status(200).send("Curso de Node")
-})
-
-app.get('/books', (req, res) => {
-  livros.find((err, livros) => {
-    res.status(200).json(livros)
-  })
-})
-
-app.post('/book', (req, res) => {
-  books.push(req.body)
-  res.status(201).send("Livro cadastrado com sucesso")
-})
-
-app.put('/book/:id', (req, res) => {
-  let index = searchBook(req.params.id)
-
-  books[index].titulo = req.body.titulo
-  res.json(books)
-})
-
-app.get('/book/:id', (req, res) => {
-  let index = searchBook(req.params.id)
-
-  res.json(books[index])
-})
-
-app.delete('/book/:id', (req, res) => {
-  let index = searchBook(req.params.id)
-
-  books.splice(index, 1)
-  res.json(books)
-})
-
-function searchBook(id) {
-  return books.findIndex(livro => livro.id == id)
-}
+routes(app);
 
 export default app;
---
+++ src/routes/livrosRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import LivroController from "../controllers/livrosController.js";
+
+const router = express.Router();
+
+router.get("/livros", LivroController.listarLivros)
+router.post("/livros", LivroController.cadastrarLivro)
+router.put("/livros/:id", LivroController.atualizarLivro)
+router.get("/livros/:id", LivroController.listarLivroPorId)
+router.delete("/livros/:id", LivroController.excluirLivro)
+
+export default router;
